6f7cc78c437a09db14b37895939a667c94d879da
Yaroslav Pershin <62902094+iapershin@users.noreply.github.com>
2025-02-14 13:22:48 +0300

    fix: fix hook panic
    Signed-off-by: Yaroslav Pershin <62902094+iapershin@users.noreply.github.com>

--- internal/command/hooks.go
+++ internal/command/hooks.go
@@ -6,7 +6,7 @@ import (
 )
 
 func (e *Executor) RunOnCommandSuccessHook(cfg *config.Config) error {
-	if cfg.Hooks.OnCommandSuccess != nil {
+	if cfg.Hooks != nil && cfg.Hooks.OnCommandSuccess != nil {
 		log.Println("Running onCommandSuccess hook")
 		if err := e.Exec(*cfg.Hooks.OnCommandSuccess); err != nil {
 			return err
@@ -16,7 +16,7 @@ func (e *Executor) RunOnCommandSuccessHook(cfg *config.Config) error {
 }
 
 func (e *Executor) RunOnCommandFailureHook(cfg *config.Config) error {
-	if cfg.Hooks.OnCommandFailure != nil {
+	if cfg.Hooks != nil && cfg.Hooks.OnCommandFailure != nil {
 		log.Println("Running onCommandFailure hook")
 		if err := e.Exec(*cfg.Hooks.OnCommandFailure); err != nil {
 			return err
@@ -26,7 +26,7 @@ func (e *Executor) RunOnCommandFailureHook(cfg *config.Config) error {
 }
 
 func (e *Executor) RunOnCommandSkippedHook(cfg *config.Config) error {
-	if cfg.Hooks.OnCommandSkiped != nil {
+	if cfg.Hooks != nil && cfg.Hooks.OnCommandSkiped != nil {
 		log.Println("Running onCommandSkiped hook")
 		if err := e.Exec(*cfg.Hooks.OnCommandSkiped); err != nil {
 			return err
@@ -36,7 +36,7 @@ func (e *Executor) RunOnCommandSkippedHook(cfg *config.Config) error {
 }
 
 func (e *Executor) RunOnQuorumFailedHook(cfg *config.Config) error {
-	if cfg.Hooks.OnQuorumFailure != nil {
+	if cfg.Hooks != nil && cfg.Hooks.OnQuorumFailure != nil {
 		log.Println("Running onQuorumFailure hook")
 		if err := e.Exec(*cfg.Hooks.OnQuorumFailure); err != nil {
 			return err
